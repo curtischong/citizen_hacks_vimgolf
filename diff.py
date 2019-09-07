6e873dffe93c9d4945af6052c3fd0648423c321c
Curtis Chong <curtischong5@gmail.com>
2019-09-07 16:49:17 -0400

    made a script to make the diff files

---
+++ diff.py
@@ -0,0 +1,14 @@
+import subprocess
+
+for i in range(1,9):
+    i = str(i)
+    bashCommand = "diff chpart"+i+"a.txt chpart"+i+"p.txt"
+
+    with open("chpart"+i+"d.txt","wb") as out, open("stderr.txt","wb") as err:
+        subprocess.Popen(bashCommand.split(),stdout=out,stderr=err)
+
+    #process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+    #output, error = process.communicate()
+    #" > chpart"+i+"d.txt"
+    #print(output)
+    #print(error)
